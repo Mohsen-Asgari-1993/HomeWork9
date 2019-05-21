ce0f193134830c6e28369ec0c3f5856a1a077a51
mohsen <mohsen1993asgari@gmail.com>
2019-05-21 11:10:37 +0430

    save instance of student in DB

--- src/main/java/com/maktab/App.java
+++ src/main/java/com/maktab/App.java
@@ -1,11 +1,9 @@
 package com.maktab;
 
-import com.maktab.domain.Teacher;
-import com.maktab.repository.impl.TeacherDAOImpl;
+import com.maktab.domain.Student;
+import com.maktab.repository.impl.StudentDAOImpl;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-import java.time.LocalDate;
 
 /**
  * Hello world!
@@ -13,12 +11,17 @@ import java.time.LocalDate;
 public class App {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
-        TeacherDAOImpl teacherDAOImpl = new TeacherDAOImpl(factory);
+//        TeacherDAOImpl teacherDAOImpl = new TeacherDAOImpl(factory);
+//
+//        teacherDAOImpl.create(new Teacher(0L, "ali", "mohammadi", 1454L, LocalDate.of(1989, 5, 14), 145L));
+//        teacherDAOImpl.create(new Teacher(0L, "mohsen", "asgari", 1958L, LocalDate.of(1993, 1, 30), 2500L));
+//        teacherDAOImpl.create(new Teacher(0L, "siavash", "ahadi", 1777L, LocalDate.of(2000, 12, 1), 980L));
+//        teacherDAOImpl.create(new Teacher(0L, "akbar", "Erfani", 7852L, LocalDate.of(1977, 3, 25), 4500L));
 
-        teacherDAOImpl.create(new Teacher(0L, "ali", "mohammadi", 1454L, LocalDate.of(1989, 5, 14), 145L));
-        teacherDAOImpl.create(new Teacher(0L, "mohsen", "asgari", 1958L, LocalDate.of(1993, 1, 30), 2500L));
-        teacherDAOImpl.create(new Teacher(0L, "siavash", "ahadi", 1777L, LocalDate.of(2000, 12, 1), 980L));
-        teacherDAOImpl.create(new Teacher(0L, "akbar", "Erfani", 7852L, LocalDate.of(1977, 3, 25), 4500L));
 
+        StudentDAOImpl studentDAO = new StudentDAOImpl(factory);
+        studentDAO.create(new Student(0L, "Taghi", "Taghavi"));
+        studentDAO.create(new Student(0L, "naghi", "naghavi"));
+        studentDAO.create(new Student(0L, "ali", "alavi"));
     }
 }
