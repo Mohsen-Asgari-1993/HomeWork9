322f55e5cfb9233a572b64fb32d76715db87c44d
mohsen <mohsen1993asgari@gmail.com>
2019-05-21 09:18:27 +0430

    Adds Teacher class Define properties

---
+++ src/main/java/com/maktab/domain/Teacher.java
@@ -0,0 +1,42 @@
+package com.maktab.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Teacher")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "teacher_code", nullable = false)
+    private Long teacherCode;
+
+    @Column(name = "birth_day")
+    private LocalDate date;
+
+    @Column(name = "salary")
+    private Long salary;
+
+
+}
