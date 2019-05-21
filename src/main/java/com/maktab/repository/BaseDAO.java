49f59ee69f0de04ca14c5baa0b74c328ad68ef2d
mohsen <mohsen1993asgari@gmail.com>
2019-05-21 09:53:34 +0430

    update  base Dao

--- src/main/java/com/maktab/repository/BaseDAO.java
+++ src/main/java/com/maktab/repository/BaseDAO.java
@@ -1,4 +1,15 @@
 package com.maktab.repository;
 
-public class BaseDAO {
+import java.io.Serializable;
+
+public interface BaseDAO<E extends Serializable> {
+
+    void create(E e);
+
+    E update(E e);
+
+    E read(Serializable id);
+
+    void delete(Serializable id);
+
 }
