c47cb74ea9fcbe738329feb02eb3f3cf78556c18
mohsen <mohsen1993asgari@gmail.com>
2019-05-21 09:59:04 +0430

    Adds base DAO impl

---
+++ src/main/java/com/maktab/repository/impl/BaseDAOImpl.java
@@ -0,0 +1,10 @@
+package com.maktab.repository.impl;
+
+import com.maktab.repository.BaseDAO;
+
+import java.io.Serializable;
+
+public abstract class BaseDAOImpl<E extends Serializable> implements BaseDAO<E> {
+
+
+}
