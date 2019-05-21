4b28331d759bcad77121bf982d60254b6fdbc64d
mohsen <mohsen1993asgari@gmail.com>
2019-05-21 09:57:17 +0430

    Update student dao

--- src/main/java/com/maktab/repository/StudentDAO.java
+++ src/main/java/com/maktab/repository/StudentDAO.java
@@ -2,5 +2,12 @@ package com.maktab.repository;
 
 import com.maktab.domain.Student;
 
+import java.util.List;
+
 public interface StudentDAO extends BaseDAO<Student> {
+
+    List<Student> searchByFirstName();
+
+    List<Student> searchByLastName();
+
 }
