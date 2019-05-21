b92427ab1f06c512e2cdcaf0379682afaf9cb1d8
mohsen <mohsen1993asgari@gmail.com>
2019-05-21 09:54:57 +0430

    Update teacherDAO

--- src/main/java/com/maktab/repository/TeacherDAO.java
+++ src/main/java/com/maktab/repository/TeacherDAO.java
@@ -1,4 +1,17 @@
 package com.maktab.repository;
 
-public interface TeacherDAO {
+import com.maktab.domain.Teacher;
+
+import java.util.List;
+
+public interface TeacherDAO extends BaseDAO<Teacher> {
+
+    List<Teacher> findAll();
+
+    void deleteByTeacherCode(Long Tcode);
+
+    List<Teacher> showOldAndYoung();
+
+    List<Teacher> showMinAndMaxSalary();
+
 }
