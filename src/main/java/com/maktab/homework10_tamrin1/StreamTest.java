120ebf5315b18ecf59e7ec4293afaf20ad44e803
mohsen <mohsen1993asgari@gmail.com>
2019-05-28 10:19:08 +0430

    Creates initArray Method

--- src/main/java/com/maktab/homework10_tamrin1/StreamTest.java
+++ src/main/java/com/maktab/homework10_tamrin1/StreamTest.java
@@ -2,4 +2,11 @@ package com.maktab.homework10_tamrin1;
 
 public class StreamTest {
 
+
+    private static String[] initArray() {
+        String[] strings = {"Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali",
+                "Davood", "Reza", "Mohsen"};
+        return strings;
+    }
+
 }
