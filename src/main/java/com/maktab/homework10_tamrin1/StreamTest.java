f40f5b746f4bf4cb581de51e0d136ead9bea5e0e
mohsen <mohsen1993asgari@gmail.com>
2019-05-28 10:23:24 +0430

    Adds sort Array By Stream Method

--- src/main/java/com/maktab/homework10_tamrin1/StreamTest.java
+++ src/main/java/com/maktab/homework10_tamrin1/StreamTest.java
@@ -1,5 +1,11 @@
 package com.maktab.homework10_tamrin1;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StreamTest {
 
 
@@ -7,6 +13,12 @@ public class StreamTest {
         String[] strings = {"Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad", "Ali",
                 "Davood", "Reza", "Mohsen"};
         return strings;
+    }
+
+    private static Map<Integer, List<String>> sortArrayByStream(String[] strings) {
+        Map<Integer, List<String>> map = Arrays.stream(strings).sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.groupingBy(String::length));
+        return map;
     }
 
 }
